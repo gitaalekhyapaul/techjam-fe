61dfae90b0b9159cc1356c7c88dacc6473c828ed
Justin <justinhon8@gmail.com>
2025-08-31 06:06:49 +0800

    wip: navigation

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,16 @@
 import { TikTokSubscriptionPage } from "@/components/subs"
+import { useRouter } from "next/navigation"
 
 export default function SubscriptionPage() {
-  return <TikTokSubscriptionPage />
+  const router = useRouter()
+  return <TikTokSubscriptionPage  onNavigateToMain={ () => {
+    router.push("/")
+  } }
+  onNavigateToWallet={ () => {
+    router.push("/")
+  } }
+  onBack={ () => {  
+    router.push("/")  
+  } }
+  />
 }
